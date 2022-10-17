0d590537593b857f37c86fae7ba103d8c8a76a72
abianfikri <abronjoy46@gmail.com>
2022-10-17 21:37:36 +0700

    import apche

---
+++ src/main/java/PABD_C/Koneksi/Database_PABD/KoneksiDatabasePabdApplication.java
@@ -0,0 +1,13 @@
+package PABD_C.Koneksi.Database_PABD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoneksiDatabasePabdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KoneksiDatabasePabdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/PABD_C/Koneksi/Database_PABD/KoneksiDatabasePabdApplicationTests.java
@@ -0,0 +1,13 @@
+package PABD_C.Koneksi.Database_PABD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KoneksiDatabasePabdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
